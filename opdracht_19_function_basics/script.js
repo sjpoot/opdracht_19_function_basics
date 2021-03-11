ab975f8f5dc6912735d9f2e3339d8add43c01530
sjpoot <78816393+sjpoot@users.noreply.github.com>
2021-03-11 17:11:12 +0100

    Add files via upload

---
+++ opdracht_19_function_basics/script.js
@@ -0,0 +1,32 @@
+// a function to make pizza
+const makePizza = function () {
+    console.log("Add flower, water, oil, yeast and 1 egg together in a bowl");
+    console.log("mix it up and let it rest for a while");
+    console.log("make tomato sauce out of tomatoes, garlic, parsley and italian herbs");
+    console.log("cut an onion, bell-pepper, mushrooms in small pieces");
+    console.log("Get other ingredients you want as topping, such as olives, mozarella, gouda cheese etc.")
+    console.log("divide the dough in 6 little parts and roll them untill the are flat")
+    console.log("put all the ingredients on the flat bread")
+    console.log("shove it in the oven for 10 to 15 minutes")
+    console.log("Let's EAT!!")
+}
+
+const makeSushi = function () {
+    console.log("Buy nuri in the shop")
+    console.log("Cook the sushi rice and let cool down")
+    console.log("Get some fresh salmon and tuna and cut it in small pieces")
+    console.log("Do the same with a cucumber and avocado")
+    console.log("lay the nuri on the wrapper, cover it with rice")
+    console.log("add you ingredients on the rice and roll it up")
+    console.log("Cut the roll in smaller pieces and enjoy with some soy sauce!")
+}
+
+makeSushi();
+console.log("====================================================")
+makeSushi();
+console.log("====================================================")
+makeSushi();
+console.log("====================================================")
+makePizza();
+console.log("====================================================")
+makePizza();
